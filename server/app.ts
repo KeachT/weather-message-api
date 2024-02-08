ab0ec89a297b19dc8d27b67a0b049e2f690e81b8
KeachT <503.b03@gmail.com>
2024-02-08 23:01:05 +0900

    return json

--- server/app.ts
+++ server/app.ts
@@ -3,8 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (_req, res) => {
-  res.send("hello!");
+  res.json({ msg: "東京、今日の天気は晴れ。降水確率は10%です。" });
 });
 
 app.listen(port, () => {
