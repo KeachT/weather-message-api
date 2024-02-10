0d3aedeadd293a0cb5192baadb0f13f3ef9f4931
KeachT <503.b03@gmail.com>
2024-02-10 14:19:01 +0900

    add dotenv

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,10 @@
+import env from 'dotenv'
 import express from 'express'
 
+env.config()
+
 const app = express()
-const port = 3000
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000
 
 app.use(express.json())
 
