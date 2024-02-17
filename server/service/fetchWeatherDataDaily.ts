9be153c69d6d7e489a8fbd1c930c529d6419a275
KeachT <503.b03@gmail.com>
2024-02-17 22:24:46 +0900

    add fetchWeatherDataDaily

---
+++ server/service/fetchWeatherDataDaily.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const FORECAST_DAYS = 1
+
+/**
+ * Fetch weather data for a specific location.
+ * @param {number} latitude The latitude of the location.
+ * @param {number} longitude The longitude of the location.
+ * @returns {Promise<WeatherData | null>} The weather data or null if an error occurs.
+ */
+export const fetchWeatherDataDaily = async (
+  latitude: number,
+  longitude: number
+) => {
+  const weatherApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max&forecast_days=${FORECAST_DAYS}`
+
+  try {
+    const response = await axios.get(weatherApiUrl)
+    return response?.data?.daily || {}
+  } catch (error) {
+    console.error(`Error fetching geo data from '${weatherApiUrl}':`, error)
+    return null
+  }
+}
